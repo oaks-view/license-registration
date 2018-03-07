21f02668d302499072565552b0d38b74ac57c8bd
Moses igbuku <captainoak@gmail.com>
2018-03-07 13:35:00 +0100

    template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  collapse = true;
+  isCollapse = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+// import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { FormsModule } from '@angular/forms';
+import { LicenseRegistrationComponent } from './licenseRegistration/registration/licenseRegistration.component';
+import { SubmittedApplicationsComponent } from './licenseReviewer/submittedApplications/submittedApplications.component';
+import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
 
 import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
@@ -10,13 +17,26 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LicenseRegistrationComponent,
+    SubmittedApplicationsComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    // AngularFirestoreModule,
+    AngularFireAuthModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'license-registration', component: LicenseRegistrationComponent
+      },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'submitted-applications', component: SubmittedApplicationsComponent }
+    ]),
+    Ng4LoadingSpinnerModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
